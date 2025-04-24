da5456e858c12dc5e2beca490e6a23338aac83b1
masruk <doruk.maslen@gmail.com>
2025-04-24 18:59:39 +0800

    feat: added routing for search and upload_csv webpages into primary upload_data html

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,11 @@ def index():
 @app.route('/upload') 
 def upload_data_view():
     return render_template('upload_data.html')
+
+@app.route('/search')
+def upload_search():
+    return render_template('search.html')
+
+@app.route('/upload_csv')
+def upload_csv():
+    return render_template('upload_csv.html')
