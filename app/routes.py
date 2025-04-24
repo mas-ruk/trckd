ccf1f55ab62d6fa186278d9aa4942f721839a47d
masruk <doruk.maslen@gmail.com>
2025-04-24 15:11:10 +0800

    feat: added route to upload_data view page, and started barebones HTML & CSS

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
     return "This is text for testing!"
+
+@app.route('/upload') 
+def upload_data_view():
+    return render_template('upload_data.html')
