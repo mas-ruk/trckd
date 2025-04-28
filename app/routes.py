cb77c2eeadefda5378f6437f3ab02ef653ad77a4
masruk <doruk.maslen@gmail.com>
2025-04-28 11:45:45 +0800

    fix: git confusion in merging, fixed feature/update-requirements to be ahead of main hopefully

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,7 @@ from app import app
 @app.route('/')
 def index():
     return render_template('homepage.html')
+
+@app.route('/collection')
+def collection():
+    return render_template('visualize_data.html')
