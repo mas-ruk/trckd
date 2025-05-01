009a36f9c7bc990a176a389f7a50bd0b9b73701a
KingTako44 <danieljdclayton@gmail.com>
2025-05-01 11:50:59 +0800

    Adding setup files for SQLAlchemy and databases.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
 
-app = Flask(__name__)
+application = Flask(__name__)
+application.config.from_object(Config)
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
 
-from app import routes
+from app import routes, models
---
+++ app/models.py
@@ -0,0 +1,7 @@
+from app import db
+
+class User(db.Model):
+    user_ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False, unique=True)
