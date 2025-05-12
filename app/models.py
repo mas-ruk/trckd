6dbf30420cf444261f0e2b15b6ef413fcf18e530
Saniya chawla <24042324@student.uwa.edu.au>
2025-05-12 09:10:07 +0530

    resolved the case senstivity

--- app/models.py
+++ app/models.py
@@ -1,26 +1,22 @@
-from app import db
-
 class User(db.Model):
+    __tablename__ = 'user'  
     user_ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(100), nullable=False, unique=True) 
     username = db.Column(db.String(100), nullable=False, unique=True)  
     password = db.Column(db.String(100), nullable=False)  
-    cards = db.relationship('Card', backref='user', lazy=True) 
+    cards = db.relationship('Card', backref='user', lazy=True)  
 
     def __repr__(self):
         return f'<User {self.username}>'
 
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
 class Card(db.Model):
+    __tablename__ = 'card' 
     card_ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     type = db.Column(db.String(100))
     color = db.Column(db.String(50))
     rarity = db.Column(db.String(50))
-    user_ID = db.Column(db.Integer, db.ForeignKey('user.user_ID'), nullable=False)
+    user_ID = db.Column(db.Integer, db.ForeignKey('user.user_ID'), nullable=False)  
     
     set_code = db.Column(db.String(50))  
     set_name = db.Column(db.String(100))  
@@ -32,7 +28,6 @@ class Card(db.Model):
     power = db.Column(db.String(20))  
     toughness = db.Column(db.String(20))  
 
-  
     image_uris = db.Column(db.Text)  
     color_identity = db.Column(db.String(50))  
     lang = db.Column(db.String(10))  
